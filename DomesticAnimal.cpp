492ee79fe392427caca2d93b5c54fac06003746e
MohammadPourbakht <mohammad.poorbakhat.80@gmail.com>
2021-06-07 00:09:22 +0430

    add constructor to DomesticAnimal class

---
+++ DomesticAnimal.cpp
@@ -0,0 +1,5 @@
+#include "DomesticAnimal.h"
+
+DomesticAnimal::DomesticAnimal() : Animal(){}
+
+
---
+++ DomesticAnimal.h
@@ -0,0 +1,14 @@
+#ifndef JUNGLE_DOMESTICANIMAL_H
+#define JUNGLE_DOMESTICANIMAL_H
+
+#include "Animal.h"
+
+class DomesticAnimal: public Animal {
+
+public:
+    DomesticAnimal();
+
+};
+
+
+#endif //JUNGLE_DOMESTICANIMAL_H
